75efdd9450b38d46531489f0cd4ea889f4c5f0e3
SurfMonkey17 <sherryw17@gmail.com>
2025-01-14 12:56:45 -0600

    added groups

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player=Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     clock = pygame.time.Clock()
@@ -19,9 +24,13 @@ def main():
             #check if player has closed the window, makes close button work. 
             if event.type == pygame.QUIT:  
                 return
-        player.update(dt)
+        #player.update(dt)
+        for player in updatable: 
+            player.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen)
+        #player.draw(screen)
+        for player in drawable:
+            player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
